07492f0ebb741b501c6656c613e9db37326c4370
Jessweb87 <jprosper87@gmail.com>
2022-01-20 13:04:46 -0500

    wip

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,5 @@
+function getRecipeById(recipe_id) {
+    return Promise.resolve('hooray! you got a recipe with id')
+
+}
+module.exports= { getRecipeById }
--- api/server.js
+++ api/server.js
@@ -11,5 +11,11 @@ server.use(express.json())
 // PLUG-IN the router
 server.use('/api/recipes', recipesRouter)
 
+// Bring in the CATCH ALL ENDPOINT and put it AFTER the ENDPOINTS otherwise it will trap everything
+server.use('*', (req, res) => {
+    res.json({ api: 'up'})
+})
+
+
 // DON'T FORGET TO EXPOSE THER SERVER
 module.exports = server
